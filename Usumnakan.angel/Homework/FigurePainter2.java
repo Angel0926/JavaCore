b39ce9d4acf16c863a34a6e054aa16a9a29a77eb
User <petanj@list.ru>
2021-10-05 23:55:41 +0400

    Տնային 1,  Եթե ավելի լավ տարբերակներ կան լուծման կուզեի տեսնել

---
+++ Usumnakan.angel/Homework/FigurePainter2.java
@@ -0,0 +1,33 @@
+package Homework;
+
+public class FigurePainter2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j >= 0; j--) {
+                if (j > i)
+
+                    System.out.print(" ");
+
+                else
+
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+            for (int i = 4; i >0; i--) {
+                for (int j = 0; j <4; j++) {
+                    if(j<i-1)
+                        System.out.print(" ");
+                    else
+                        System.out.print("*");
+
+                }
+
+                System.out.println();
+            }
+        }}
+
+
+
