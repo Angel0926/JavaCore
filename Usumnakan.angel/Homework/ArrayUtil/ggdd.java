e99a494512fc6a9a7d1abd1de159651ebdc82be0
User <petanj@list.ru>
2021-10-20 00:44:02 +0400

    Merge remote-tracking branch 'origin/master'

---
+++ Usumnakan.angel/Homework/ArrayUtil/ggdd.java
@@ -0,0 +1,19 @@
+package Homework.ArrayUtil;
+
+public class ggdd {
+    public static void main(String[] args) {
+
+        char[] nums = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        int index = 0;
+        int i;
+        char odd = ' ';
+        char[] evenNums = new char[nums.length];
+
+        for (i = 0; i < nums.length; i++) {
+
+
+//chanjates tia gukam(hesa kgam) ok jpit
+
+    }
+}
+}
