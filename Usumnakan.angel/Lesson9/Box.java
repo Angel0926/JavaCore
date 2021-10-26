c365982c4c74b52fcd0c0f317ce4d4b430be8221
User <petanj@list.ru>
2021-10-26 20:54:11 +0400

    Merge remote-tracking branch 'origin/master'

--- Usumnakan.angel/Lesson9/Box.java
+++ Usumnakan.angel/Lesson9/Box.java
@@ -4,8 +4,18 @@ public class Box {
     double width;
     double height;
     double depth;
-    void volume(){
+/*void volume(){
         System.out.println("volume");
         System.out.println(width * height * depth);
+*/
+
+    double volume() {
+        return width * height * depth;
+    }
+
+    void setDim(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
     }
 }
