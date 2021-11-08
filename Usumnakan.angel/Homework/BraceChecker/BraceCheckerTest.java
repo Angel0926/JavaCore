4faa279bc00b94d6a88c2817e744317c796acac3
User <petanj@list.ru>
2021-11-08 18:42:56 +0400

    Merge remote-tracking branch 'origin/master'

--- Usumnakan.angel/Homework/BraceChecker/BraceCheckerTest.java
+++ Usumnakan.angel/Homework/BraceChecker/BraceCheckerTest.java
@@ -1,7 +1,6 @@
 package Homework.BraceChecker;
 
 public class BraceCheckerTest {
-
     public static void main(String[] args) {
         BraceChecker braceChecker = new BraceChecker("Hello {from] {Java}" );
         braceChecker.check();
