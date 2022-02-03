289a0fffe944845033333566371678ae034704bd
Anjela <petrosyan.anjela.1994@gmail.com>
2022-02-03 15:42:46 +0400

    Merge remote-tracking branch 'origin/master'

--- Usumnakan.angel/ClassWork/allLessons/lesson1/HJBB.java
+++ Usumnakan.angel/ClassWork/allLessons/lesson1/HJBB.java
@@ -8,6 +8,7 @@ public class HJBB {
     public static void main(String[] args) {
         String path="C:\\Users\\User\\Desktop\\khhj.txt";
         File file=new File(path);
+        int k;
         System.out.println(file.isFile());
     }
 }
